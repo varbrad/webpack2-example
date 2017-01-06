8101c39711f1a53c52770ec292b1d9d6bbdb8f0f
Bradley Davies <davb3_15@uni.worc.ac.uk>
2017-01-06 16:41:32 +0000

    :lipstick: App styling changes

--- src/components/view-home.js
+++ src/components/view-home.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 module.exports = Vue.component('view-home', {
   template: `<div>
     <h1>{{ message }}</h1>
+    <input v-model="message">
   </div>`,
   data: function () {
     return {
